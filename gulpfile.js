900e6ca2f05d1a227a07474b81e8b2dbe2da9289
Voles <niels.dequeker@gmail.com>
2014-02-03 20:55:46 +0100

    Concatenate js files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var recess = require('gulp-recess')
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 gulp.task('default', function () {
   console.log('Init default task');
@@ -13,5 +14,11 @@ gulp.task('css', function () {
     .pipe(less())
     .pipe(minifyCss())
     .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js', function () {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
